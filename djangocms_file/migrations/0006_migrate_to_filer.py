6d4c1e09645717f727e2f6083c5b3304e3d97b8f
Dag Königrör <dkoenigroer@hatraco.de>
2020-02-12 11:54:00 +0100

    Filer migration added prints and remove-file-line.

--- djangocms_file/migrations/0006_migrate_to_filer.py
+++ djangocms_file/migrations/0006_migrate_to_filer.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import django.db.models.deletion
-from django.db import migrations, models
+import os
 
+import django.db.models.deletion
 import filer.fields.file
+from django.conf import settings
+from django.db import migrations
 
 
 def migrate_to_filer(apps, schema_editor):
@@ -16,6 +18,8 @@ def migrate_to_filer(apps, schema_editor):
     for plugin in plugins:  # pragma: no cover
         if plugin.file:
             filename = plugin.file.name.split('/')[-1]
+            old_path = os.path.join(settings.MEDIA_ROOT, str(plugin.file))
+            print("Will use {} as filename.".format(filename))
             filesrc = File.objects.get_or_create(
                 file=plugin.file.file,
                 defaults={
@@ -23,10 +27,12 @@ def migrate_to_filer(apps, schema_editor):
                 }
             )[0]
             plugins.filter(pk=plugin.pk).update(file_src=filesrc)
+            try:
+                os.remove(old_path)
+            except: pass
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('filer', '0006_auto_20160623_1627'),
         ('djangocms_file', '0005_auto_20160119_1534'),
@@ -36,7 +42,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='file',
             name='file_src',
-            field=filer.fields.file.FilerFileField(related_name='+', on_delete=django.db.models.deletion.SET_NULL, verbose_name='File', blank=True, to='filer.File', null=True),
+            field=filer.fields.file.FilerFileField(related_name='+', on_delete=django.db.models.deletion.SET_NULL,
+                                                   verbose_name='File', blank=True, to='filer.File', null=True),
         ),
         migrations.RunPython(migrate_to_filer),
         migrations.RemoveField(
